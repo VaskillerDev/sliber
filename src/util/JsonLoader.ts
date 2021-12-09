0c7977889834c995b55b12577274d6adb69d1ddf
VaskillerDev <Vaskoooo9241@gmail.com>
2021-12-10 02:46:37 +0300

    init

---
+++ src/util/JsonLoader.ts
@@ -0,0 +1,15 @@
+﻿import fs, {PathLike} from 'fs'
+
+export default class JsonLoader {
+    public static async fromFile(path: PathLike) : Promise<any> {
+        let buf = await fs.promises.readFile(path);
+        let str = buf.toString('utf-8');
+        return JSON.parse(str);
+    }
+
+    public static fromFileSync(path: PathLike) : any {
+        let buf = fs.readFileSync(path);
+        let str = buf.toString('utf-8');
+        return JSON.parse(str);
+    }
+}
