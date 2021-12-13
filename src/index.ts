956695518b7d865d1560bb700f452b86fabdaa6b
VaskillerDev <Vaskoooo9241@gmail.com>
2021-12-13 04:06:50 +0300

    rewrite

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@
 import MultipartPlugin from "fastify-multipart";
 
 import App from "./app/App";
-import HostConfig from "./configs/HostConfig";
 import RouteHandlerMap from "./handlers/RouteHandlerMap";
-import GoogleAuth from "./app/oauth2/GoogleAuth";
-import JsonLoader from "./util/JsonLoader";
 import ConfigLoader from "./util/ConfigLoader";
-import GoogleOAuthConfig from "./configs/GoogleOAuthConfig";
+import RouteHandlerOwnerMap from "./handlers/owner/RouteHandlerOwnerMap";
+import injectRouteHandlerMap from "./util/injectRouteHandlerMap";
 
 let server : FastifyInstance = Server({logger: true});
 server.register(MultipartPlugin);
@@ -15,23 +13,11 @@ server.register(MultipartPlugin);
 const configLoader = new ConfigLoader();
 const appConfig = configLoader.fromFileSync('./config.dev.json');
 
-const helloWorldHandler = RouteHandlerMap["/"];
-const uploadHandler = RouteHandlerMap["/upload"];
-
 const app = new App(server);
 app.setHostConfig(appConfig);
 app.setGoogleAuth(appConfig)
-app.setHandler(helloWorldHandler);
-app.setHandler(uploadHandler);
+
+injectRouteHandlerMap(app, RouteHandlerMap);
+injectRouteHandlerMap(app, RouteHandlerOwnerMap);
+
 app.listen();
-
-/*
-const hostCfg = new HostConfig(7070);
-
-
-const app = new App(server);
-app.setHostConfig(hostCfg);
-app.setHandler(helloWorldHandler);
-app.setGoogleAuth(credJson.web);
-app.setHandler(uploadHandler);
-app.listen();*/
