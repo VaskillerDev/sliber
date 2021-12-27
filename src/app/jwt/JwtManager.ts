39bca5d2f3086e763bedce97ce67f7ec9b2d92c8
VaskillerDev <Vaskoooo9241@gmail.com>
2021-12-27 06:49:37 +0300

    init jwt

---
+++ src/app/jwt/JwtManager.ts
@@ -0,0 +1,62 @@
+﻿import SecurityConfig from "../../configs/SecurityConfig";
+import Owner from "../owner/Owner";
+import Jwt, {JwtHeader} from "./Jwt";
+import crypto, {Cipher, Decipher} from "crypto";
+
+const JWT_HEADER : JwtHeader = {
+    alg: "HS256",
+    typ: "JWT"
+};
+
+const HASH_ALGO = 'sha256';
+
+export default class JwtManager {
+
+    #securityConfig : SecurityConfig;
+    
+    constructor(args: JwtManagerArgs) {
+        const {securityConfig} = args;
+        
+        this.#securityConfig = securityConfig;
+    }
+    
+    public createJwtFromOwner(owner : Owner) : Jwt {
+        return new Jwt(JWT_HEADER, owner, () => {
+            return owner.getName() + owner.getOwnerId();
+        });
+    }
+
+    private generateCipher(code: string) : Cipher {
+        const algo = this.#securityConfig.algorithm;
+        const initVec = this.#securityConfig.initVector;
+        
+        const key = JM.generateHash(code).substr(0, 32);
+        const cipher = crypto.createCipheriv(algo, key, initVec);
+
+        return cipher;
+    }
+    
+    private generateDecipher(code: string) : Decipher {
+        const algo = this.#securityConfig.algorithm;
+        const initVec = this.#securityConfig.initVector;
+
+        const key = JM.generateHash(code).substr(0, 32);
+        const decipher = crypto.createDecipheriv(algo, key, initVec);
+
+        return decipher;
+    }
+    
+    private static generateHash(str : string) : string {
+        return crypto
+            .createHash(HASH_ALGO)
+            .update(str)
+            .digest('hex');
+    }
+    
+}
+
+const JM = JwtManager;
+
+export interface JwtManagerArgs {
+    securityConfig: SecurityConfig
+}
---
+++ src/util/jwt/fromUtf8ToBase64Url.ts
@@ -0,0 +1,9 @@
+﻿export default function fromUtf8ToBase64Url(str: string) : string {
+    const strAsBase64 = Buffer
+        .from(str, 'utf-8')
+        .toString('base64');
+    return strAsBase64
+        .replace(/\+/g, '-')
+        .replace(/\\/g, '_')
+        .replace(/=/g, '');
+}
